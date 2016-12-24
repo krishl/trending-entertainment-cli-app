44f5b1d5b764d5c56d635e0c7f1941faef41c6ef
Krishel Lasam <krishel.lasam@gmail.com>
2016-12-24 00:43:40 -0500

    add CLI.shows_or_movies

--- lib/trending_entertainment_cli_app/CLI.rb
+++ lib/trending_entertainment_cli_app/CLI.rb
@@ -1,5 +1,25 @@
 class TrendingEntertainmentCliApp::CLI
   def call
-    puts "Welcome! What would you like to explore, TV Shows or Movies?"
+    shows_or_movies
   end
+
+  def shows_or_movies
+    answer = nil
+    while answer != "exit"
+      puts "Welcome! What would you like to explore, TV Shows or Movies? Type 'exit' to exit."
+      answer = gets.chomp.downcase
+      case answer
+      when "tv shows", "tv", "shows", "tvshows", "tvshow", "show"
+        puts "shows"
+      when "movies", "movie"
+        puts "movies"
+      when "exit"
+        puts "Have a nice day!"
+      else
+        puts "Please enter a valid input!"
+      end
+    end
+  end
+
+
 end
