c44de9dec3226762cdf21b62f54de1edc3a72288
Krishel Lasam <krishel.lasam@gmail.com>
2016-12-24 01:17:37 -0500

    rename CLI.call to CLI.main_menu, add CLI.shows and CLI.farewell methods

--- lib/trending_entertainment_cli_app/CLI.rb
+++ lib/trending_entertainment_cli_app/CLI.rb
@@ -1,24 +1,43 @@
 class TrendingEntertainmentCliApp::CLI
-  def call
-    shows_or_movies
-  end
-
-  def shows_or_movies
+  def main_menu
     answer = nil
     while answer != "exit"
       puts "Welcome! What would you like to explore, TV Shows or Movies? Type 'exit' to exit."
       answer = gets.chomp.downcase
       case answer
       when "tv shows", "tv", "shows", "tvshows", "tvshow", "show"
-        puts "shows"
+        shows
       when "movies", "movie"
-        puts "movies"
+        puts "movies" #placeholder for .movies
       when "exit"
-        puts "Have a nice day!"
+        farewell
       else
         puts "Please enter a valid input!"
       end
     end
+  end
+
+  def shows
+    answer = nil
+    while answer != "back"
+      puts "Would you like to (1) view currently trending TV Shows or (2) view a list of the most anticipated upcoming shows?"
+      puts "Make your selection by typing either '1' or '2'. Alternatively, type 'back' to go back to the main menu."
+      answer = gets.chomp
+      case answer
+      when "1"
+        puts "trending_shows" #placeholder for Shows.trending_shows
+      when "2"
+        puts "anticipated_shows" #placeholder for Shows.anticipated_shows
+      when "back"
+        main_menu
+      else
+        puts "Please enter a valid input!"
+      end
+    end
+  end
+
+  def farewell
+    puts "Have a nice day!"
   end
 
 
